459a08bb040bc4db1b68958806852cae144329f8
DonnMurphy <116433164@umail.ucc.ie>
2020-02-19 23:08:18 +0000

    adapting codebase for heroku deployment

---
+++ routes/posts.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+
+// Route Returns All Posts
+router.get('/', async (req,res) => {
+  try{
+    const posts = await Post.find(); //reserarch the .find mongoose method for limits/ querys etc
+    res.json(posts);
+  }catch (err){
+    res.json({message:err});
+  }
+});
+
+// Route Returns Specifc Post
+router.get('/:postId', async (req,res) => {
+  //res.send('We are on specific!');
+  //console.log(req.params.postId);
+  try{
+  const post = await Post.findById(req.params.postId);
+  res.json(post);
+}catch(err){
+  res.json({message:err});
+  }
+});
+
+// Route Submits New Post
+router.post('/', async (req,res) => {
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description
+  });
+
+  try{
+    const savedPost = await post.save();
+    res.json(savedPost);
+  }catch (err){
+    res.json({message: err});
+  }
+  //console.log(req.body);
+});
+
+//Delete POst
+router.delete('/:postId',async (req,res) => {
+  try{
+    const removedPost = await Post.remove({_id});
+    res.json(removedPost);
+  }catch(err){
+    res.json({message:err});
+  }
+});
+
+//update a post
+router.patch("/:postId", async (req,res) => {
+  try{
+  const updatedPost = await Post.updateOne({_id: res.param.postId}, {$set: {title:req.body.title }});
+  res.json(updatedPost);
+  }catch(err){
+  res.json({message:err});
+}
+})
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,109 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+// Route Returns All Users
+router.get('/', async (req,res) => {
+  try{
+    const users = await User.find(); //reserarch the .find mongoose method for limits/ querys etc
+    res.json(users);
+  }catch (err){
+    res.json({message:err});
+  }
+});
+
+// Route Returns Specifc User (public details)
+router.get('/:userId', async (req,res) => {
+  //res.send('We are on specific!');
+  //console.log(req.params.userId);
+  try{
+  const user = await User.findById(req.params.userId);
+  res.json(user);
+}catch(err){
+  res.json({message:err});
+  }
+});
+
+// Route Returns Specifc User
+router.get('/owner/:ownerId', async (req,res) => {
+  //res.send('We are on specific!');
+  //console.log(req.params.userId);
+  try{
+  const user = await User.findById(req.params.userId);
+  res.json(user);
+}catch(err){
+  res.json({message:err});
+  }
+});
+// Route creates a new user (Sign Up)
+router.post('/', async (req,res) => {
+  const user = new User({
+    title: req.body.title,
+    description: req.body.description
+  });
+
+  try{
+    const savedUser = await user.save();
+    res.json(savedUser);
+  }catch (err){
+    res.json({message: err});
+  }
+  //console.log(req.body);
+});
+
+// Route attemps login for user
+router.post('/login/:userId', async (req,res) => {
+  const user = new User({
+    title: req.body.title,
+    description: req.body.description
+  });
+
+  try{
+    const savedUser = await user.save();
+    res.json(savedUser);
+  }catch (err){
+    res.json({message: err});
+  }
+  //console.log(req.body);
+});
+
+//update a user details - check password first
+router.patch("/:userId", async (req,res) => {
+  try{
+  const updatedUser = await User.updateOne({_id: res.param.userId}, {$set: {title:req.body.title }});
+  res.json(updatedUser);
+  }catch(err){
+  res.json({message:err});
+}
+})
+
+// --------------------------- Figure everything below later -------------------
+
+// Route Submits New User
+router.post('/', async (req,res) => {
+  const user = new User({
+    title: req.body.title,
+    description: req.body.description
+  });
+
+  try{
+    const savedUser = await user.save();
+    res.json(savedUser);
+  }catch (err){
+    res.json({message: err});
+  }
+  //console.log(req.body);
+});
+
+
+//update a user -- Dont need this really?
+router.patch("/:userId", async (req,res) => {
+  try{
+  const updatedUser = await User.updateOne({_id: res.param.userId}, {$set: {title:req.body.title }});
+  res.json(updatedUser);
+  }catch(err){
+  res.json({message:err});
+}
+})
+
+module.exports = router;
